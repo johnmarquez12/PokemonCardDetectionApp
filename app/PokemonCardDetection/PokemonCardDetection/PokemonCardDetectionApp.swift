d7c52d56039622d2aa3b7b809a1c6cb0269f116d
johnmarquez12 <ajxz12@gmail.com>
2024-10-30 16:15:07 -0700

    created new requirements and converted pytorch model to coreml

---
+++ app/PokemonCardDetection/PokemonCardDetection/PokemonCardDetectionApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokemonCardDetectionApp.swift
+//  PokemonCardDetection
+//
+//  Created by John Marquez on 2024-10-30.
+//
+
+import SwiftUI
+
+@main
+struct PokemonCardDetectionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
